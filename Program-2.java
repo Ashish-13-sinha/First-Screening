6967bc315b9f457381331dc871452ee449d8ef2f
Ashish Sinha <74908203+Ashish-13-sinha@users.noreply.github.com>
2025-12-18 13:44:13 +0530

    Create Program-2.java

---
+++ Program-2.java
@@ -0,0 +1,18 @@
+public class Main{
+  public List<Integer>generatedValue(int a){
+    List<Integer>ans=new ArrayList<>();
+    for(int i=0;i<a;i++){
+      ans.add(i+(i+1))
+    }
+    return ans;
+  }
+  public static void main(String []args){
+    Scanner sc =new Scanner(System.in);
+    int a=sc.nextInt();
+    List<Integer>ans=generatedValue(a);
+    for(int i=0;i<ans.size();i++){
+      System.out.print(ans.get(i)+",");
+    }
+    
+  }
+}
